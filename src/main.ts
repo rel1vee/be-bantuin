e4c6eb735215f68eea5de8735e2c5fcf336ed48c
M-Adityarnld <aditadalahorang@gmail.com>
2025-12-17 04:08:07 +0700

    add helmet in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import morgan from 'morgan';
 import pino from 'pino';
 import pinoHttp from 'pino-http';
 import * as Sentry from '@sentry/nestjs';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const isProduction = process.env.NODE_ENV === 'production';
@@ -45,6 +46,37 @@ async function bootstrap() {
     dsn: 'https://example@sentry.io/1234567', // dns asli nyusul
     tracesSampleRate: 1.0,
   });
+
+  app.use(
+    helmet({
+      frameguard: {
+        action: 'sameorigin', // atau 'deny'
+      },
+      xssFilter: true,
+      noSniff: true,
+      hidePoweredBy: true,
+      referrerPolicy: {
+        policy: 'no-referrer',
+      },
+      hsts: {
+        maxAge: 31536000, // 1 tahun
+        includeSubDomains: true,
+        preload: true,
+      },
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'default-src': ["'self'"],
+          'script-src': ["'self'"],
+          'style-src"': ["'self'", "'unsafe-inline'"],
+          'img-src': ["'self'", 'data:'],
+          'font-src': ["'self'"],
+          'connect-src': ["'self'"],
+          'frame-ancestors': ["'self'"],
+        },
+      },
+    }),
+  );
 
   // Enable CORS Dinamis (FIXED TYPE SAFE)
   app.enableCors({
